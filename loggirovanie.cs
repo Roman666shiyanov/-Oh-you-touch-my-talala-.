dc4e91e38333a11dcff9e87880ed4c893121cf6a
Roman666shiyanov <60029536+Roman666shiyanov@users.noreply.github.com>
2021-06-29 01:16:28 +0300

    Add files via upload

---
+++ loggirovanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Витрина
+{
+    public class Log
+    {
+        public static void Logger(string text)
+        {
+			// казываем путь для записи
+            string path = "C:\\loggirovanie.txt";
+            File.AppendAllText(path, text + " " + DateTime.Now + "\n");
+
+        }
+    }
+}
